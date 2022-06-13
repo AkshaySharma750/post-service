5cbd7d5204d27bd0ddb06350d6fd425884eeeb51
Akshay <akshaysharma750@gmail.com>
2022-06-13 11:00:52 +0530

    initial commit

---
+++ src/main/java/com/myfb/postservice/entity/PostEntity.java
@@ -0,0 +1,22 @@
+package com.myfb.postservice.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Post_Table")
+@Setter
+@Getter
+@NoArgsConstructor
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String body;
+    private Long userId;
+}
